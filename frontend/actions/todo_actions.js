e3b37d89f78ab86d291fccbdf4313c8626d68f20
sameehkhan <sameehkhan1@gmail.com>
2018-12-03 14:09:02 -0800

    Receive todos and todo action

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -2,7 +2,12 @@ export const RECIEVE_TODOS = 'RECEIVE_TODOS';
 export const RECEIVE_TODO = 'RECEIVE_TODO';
 
 
-export const receiveTodos = message => ({
+export const receiveTodos = (todos) => ({ // This "todos" variable is an array AND the receiveTodos is the action!
   type: RECEIVE_TODOS,
-  message: message
+  todos: todos // [Do the dishes, take out the trash]
+});
+
+export const receiveTodo = (todo) => ({
+  type: RECEIVE_TODO,
+  todo: todo
 });
