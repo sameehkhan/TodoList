d2c86d9a609d253c194de1f6f9de82afcbfd4ef8
sameehkhan <sameehkhan1@gmail.com>
2018-12-03 15:22:04 -0800

    Fix typos

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,6 +1,5 @@
-export const RECIEVE_TODOS = 'RECEIVE_TODOS';
+export const RECEIVE_TODOS = 'RECEIVE_TODOS';
 export const RECEIVE_TODO = 'RECEIVE_TODO';
-
 
 export const receiveTodos = (todos) => ({ // This "todos" variable is an array AND the receiveTodos is the action!
   type: RECEIVE_TODOS,
--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,6 +1,22 @@
-import {RECEIVE_TODOS, RECEIVE_TODO} from '../actons/todo_actions';
+import {RECEIVE_TODOS, RECEIVE_TODO} from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+// reducers/todos_reducer.js
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
   switch(action.type) {
     default:
       return state;
