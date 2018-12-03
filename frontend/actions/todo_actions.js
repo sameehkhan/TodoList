635b3540057ebf11e8a272bbb28745913708be5c
sameehkhan <sameehkhan1@gmail.com>
2018-12-03 13:54:05 -0800

    Export receive todos function

--- frontend/actions/todo_actions.js
+++ frontend/actions/todo_actions.js
@@ -1,2 +1,8 @@
 export const RECIEVE_TODOS = 'RECEIVE_TODOS';
 export const RECEIVE_TODO = 'RECEIVE_TODO';
+
+
+export const receiveTodos = message => ({
+  type: RECEIVE_TODOS,
+  message: message
+});
