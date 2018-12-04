49fe5ffb8fe336f53f6e09f6f28bd3eb611ad9e2
sameehkhan <sameehkhan1@gmail.com>
2018-12-04 15:00:57 -0800

    Import allTodos object

--- todo_redux.jsx
+++ todo_redux.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './frontend/store/store';
 import App from './frontend/components/app';
 import Root from './frontend/components/root';
+import allTodos from './frontend/reducers/selectors';
 
 document.addEventListener("DOMContentLoaded", function(event) {
   window.store = configureStore();
