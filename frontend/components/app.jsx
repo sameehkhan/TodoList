69327c96bb274dd829b35a83e137a18b0bb157ca
sameehkhan <sameehkhan1@gmail.com>
2018-12-04 14:15:51 -0800

    Add React component directory

---
+++ frontend/components/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+  return (
+  <div>
+    <h1>Where you get to where I'm at</h1>
+    <h2>you gotta remind to where the f you at</h2>
+  </div>
+);
+  }
+}
+
+export default App;
