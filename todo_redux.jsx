be5aee7d65408db20daa2f3acace142899cfae4c
sameehkhan <sameehkhan1@gmail.com>
2018-12-04 14:32:00 -0800

    Add root to render

--- todo_redux.jsx
+++ todo_redux.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './frontend/store/store';
 import App from './frontend/components/app';
+import Root from './frontend/components/root';
 
 document.addEventListener("DOMContentLoaded", function(event) {
   window.store = configureStore();
 });
 
 const Content = document.getElementById('content');
-ReactDOM.render(<App/>, Content);
+ReactDOM.render(<Root/>, Content);
