68f10bd8cc6a6e2b98e6aabd3bb9e4b94588145a
sameehkhan <sameehkhan1@gmail.com>
2018-12-04 14:17:21 -0800

    Import App, render App React component

--- todo_redux.jsx
+++ todo_redux.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './frontend/store/store';
+import App from './frontend/components/app';
 
 document.addEventListener("DOMContentLoaded", function(event) {
   window.store = configureStore();
 });
+
+const Content = document.getElementById('content');
+ReactDOM.render(<App/>, Content);
